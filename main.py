295d071104c8dc8358161be1c9c58c647ce54441
kotwys <52920928+kotwys@users.noreply.github.com>
2019-08-19 14:36:51 +0400

    Parse command-line options

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from sys import argv
 from process import process
 
 def main():
+    options = argv[1:]
+
+    if not options:
+        print('No script specified')
+        return
+
+    script = options[0]
+
+    if len(options) > 1:
+        output = options[1]
+    else:
+        output = 'auto'
+
     process()
 
 if __name__ == "__main__":
