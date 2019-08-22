8a88abf8d6bf0cadcdee9562fa6bd44c649090d9
kotwys <52920928+kotwys@users.noreply.github.com>
2019-08-22 14:03:44 +0400

    merge: Switch to ImageMagick (Wand)
    Use Wand (ImageMagick wrapper) to generate graphics.
    
    Also now draw() function itself generates images, therefore
    size and `mode=` properties of register() have been removed.

--- examples/color_props.py
+++ examples/color_props.py
@@ -1,21 +1,31 @@
-from schema import Schema, And, Use
+from schema import Schema, And, Or, Use
 
-def draw(im, state):
-    im.rectangle(
-        [10, 10, 290, 290],
-        fill=state['color']
-    )
+from wand.image import Image
+from wand.color import Color
+from wand.drawing import Drawing
+
+def draw(state):
+    image = Image(
+        width=300, height=300,
+        background=Color('black')
+    ) 
+
+    with Drawing() as draw:
+        draw.fill_color = Color(state['color'])
+        draw.rectangle(
+            left=10, top=10,
+            right=290, bottom=290
+        )
+        draw(image)
+    
+    return image
 
 
 def init(props):
-    return {
-        'color': tuple(props['color'])
-    }
+    return props
 
 register(
-    (300, 300),
-    mode='RGB',
     schema=Schema({
-        'color': And(Use(tuple), lambda x: len(x) >= 3)
+        'color': Or(str, Use(lambda x: 'rgb({},{},{})'.format(*x)))
     }),
 )
